8b8842056059105d87a694cad4e05c055b473ceb
kafagy95 <alikafag@buffalo.edu>
2017-09-22 22:53:23 -0700

    Parsing Final Player Ratings and Making Minor Fixes and Optimizations

---
+++ README.md
@@ -0,0 +1,21 @@
+#FIFA 18 FUT Players Data
+
+##What is the purpose of this script?
+- The purpose of this repository is to have a script that autoamtically pulls down all players data from Futhead's website
+
+##Players data ouput format:
+- After running the script the user will have the data saved in a MySQL databse and a CSV file
+
+#Requirements:
+- Python 3.xx
+- MySQL
+- PIP
+
+
+###You will need to use PIP to install the following libraries:
+- pymysql
+- bs4
+- requests
+
+####Note:
+- Make sure you create table fifa18 using the DDL in fifa.sql to be able to correctly encode latin literals when writing to the Database
--- fifa18.sql
+++ fifa18.sql
@@ -1,25 +1,25 @@
--- Drop fifa18 table
-DROP TABLE fifa18;
+-- Drops fifa18 table
+-- DROP TABLE fifa18;
 
--- Truncate fifa18 table
-TRUNCATE TABLE fifa18;
+-- Truncates fifa18 table
+--TRUNCATE TABLE fifa18;
 
 -- DDL for fifa18 table
 CREATE TABLE fifa18
 (
   names     CHAR(30) CHARACTER SET latin1 COLLATE latin1_bin,
-  club      varchar(30) ,
-  league    varchar(30),
-  position  varchar(30),
+  club      varchar(50),
+  league    varchar(50),
+  position  varchar(50),
   rating    INTEGER(2),
   pace      INTEGER(2),
   shooting  INTEGER(2),
   passing   INTEGER(2),
-  dribbling INTEGER(22),
-  defending INTEGER(22),
-  physical  INTEGER(22),
+  dribbling INTEGER(2),
+  defending INTEGER(2),
+  physical  INTEGER(2),
   loaddate  timestamp
 );
 
 -- Insert statement example
-INSERT INTO futhead.fifa18 (names, club, league, position, rating, pace, shooting, passing, dribbling, defending, physical, loaddate) VALUES ("Alexander Esswein", "Hertha Berlin", "Bundesliga", "RM", 73, 89, 72, 65, 72, 31, 75, NOW());
+-- INSERT INTO futhead.fifa18 (names, club, league, position, rating, pace, shooting, passing, dribbling, defending, physical, loaddate) VALUES ("Alexander Esswein", "Hertha Berlin", "Bundesliga", "RM", 73, 89, 72, 65, 72, 31, 75, NOW());
